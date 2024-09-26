04b5ae79ee99e99346f54d96952b68621ba11e77
aymnn <ayman.belahsen.4@gmail.com>
2024-09-26 17:22:44 +0100

    1. MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,41 @@
+import mongodb from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL,
+      { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  async findUserByEmailAndPassword(email, password) {
+    return this.client.db().collection('users').findOne({ email, password });
+  }
+
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
