4b68ae2123d5310b4a7bc2b033559e48c5be1962
aymnn <ayman.belahsen.4@gmail.com>
2024-09-26 17:40:15 +0100

    2. First API

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).json({
+      users,
+      files,
+    });
+  }
+}
+
+export default AppController;
